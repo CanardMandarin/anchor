9c7b5ff5cff410e4c435cd33766a163bcc7b7c54
CanardMandarin <thibault@opcodes.fr>
2023-05-08 14:22:25 +0000

    fix equality check + typo

--- lang/syn/src/parser/accounts/mod.rs
+++ lang/syn/src/parser/accounts/mod.rs
@@ -183,15 +183,16 @@ fn constraints_cross_checks(fields: &[AccountField]) -> ParseResult<()> {
                 InitKind::Mint { .. } => {
                     if init_fields.iter().enumerate().any(|(f_pos, f)| {
                         match &f.constraints.init.as_ref().unwrap().kind {
-                            InitKind::Token { mint, .. } | InitKind::AssociatedToken { mint, .. } => {
-                                mint.to_token_stream().to_string().starts_with(&field.ident.to_string()) && pos > f_pos
+                            InitKind::Token { mint, .. }
+                            | InitKind::AssociatedToken { mint, .. } => {
+                                field.ident == mint.to_token_stream().to_string()  && pos > f_pos
                             }
-                            _ => false
+                            _ => false,
                         }
                     }) {
                         return Err(ParseError::new(
                             field.ident.span(),
-                            "because of the init constraint, the mint has to be declared before any corresponding token account",
+                            "because of the init constraint, the mint has to be declared before the corresponding token account",
                         ));
                     }
                 }
